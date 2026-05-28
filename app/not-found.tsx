da1240a1defd4e223692e63c3a3b2b1f1c1bfa6d
Andriy_Chornobay <andriy.chornobay@gmail.com>
2026-05-28 16:45:02 +0300

    add initial files

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,30 +4,13 @@ import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'NotHub - Page Not Found',
-  description: 'The page you are looking for does not exist.',
-  openGraph: {
-    title: 'NotHub - Page Not Found',
-    description: 'The page you are looking for does not exist.',
-    url: 'https://notehub.app/not-found',
-    images: [
-      {
-        url: '/page-note-found.jpeg',
-        width: 1200,
-        height: 630,
-        alt: 'page not found',
-      },
-    ],
-  },
-};
-
 const NotFound = () => {
   const router = useRouter();
   useEffect(() => {
     const timer = setTimeout(() => {
-      () => router.push('/');
+      router.push('/');
     }, 3000);
+
     return () => clearTimeout(timer);
   }, [router]);
   return (
