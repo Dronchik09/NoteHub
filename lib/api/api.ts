a978f230fb23037b36e802f5f78677ac90c1cbfc
Andriy_Chornobay <andriy.chornobay@gmail.com>
2026-05-28 17:05:48 +0300

    add initial files

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
+  // process.env.NEXT_PUBLIC_API_URL +
+  baseURL: '/api',
   withCredentials: true,
 });
 //const myToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
